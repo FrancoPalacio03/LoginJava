4134277c07d4c457f9847cd532ff28ad8940b3cc
FrancoPalacio <francopalacio03@gmail.com>
2023-08-14 12:08:41 -0300

    commit

---
+++ src/main/java/com/mycompany/loginjava/LoginJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginjava;
+
+/**
+ *
+ * @author franc
+ */
+public class LoginJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
